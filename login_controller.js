41bf71e5a33a9b85350a06f46ea5549bc03d932d
ben smith <benjaminsmith1981@gmail.com>
2017-07-31 15:48:01 +0200

    Updated update to work for updating login details

--- login_controller.js
+++ login_controller.js
@@ -5,7 +5,7 @@ const nodemailer = require('nodemailer');
 
 exports.verifymember = function(req, res, err) {
     var email = req.body.email //becomes user name
-    var familyTreeID = req.body.patientID
+    var patientID = req.body.patientID
     var userID = req.body.userID
     var passwordCode = generateCode()
     var sendersEmail = req.body.sendersEmail
@@ -48,7 +48,7 @@ exports.verifymember = function(req, res, err) {
     var login = new LoginSchema({  
         username: email,
         password: passwordCode,
-        familyTreeID: familyTreeID,
+        patientID: patientID,
         id: userID
     })
     LoginSchema.find({username: req.body.email}, function(err, doc){ //function (err, callback) {
@@ -75,8 +75,8 @@ function generateCode(){
 }
 
 exports.addpatientsid = function(req, res, err) {
-    LoginSchema.updateOne({id: req.body.username }, 
-        {$set: {familyTreeID:req.body.patientID, id: req.body.patientID} }, 
+    LoginSchema.updateOne({username: req.body.username }, 
+        {$set: {patientID:req.body.patientID, id: req.body.patientID} }, 
         {upsert: true}, 
         function(err, callback){
             console.log("callback.id " + callback.id)
@@ -97,7 +97,7 @@ exports.register = function(req, res, err) {
     var login = new LoginSchema({  
         username: req.body.username,
         password: req.body.password,
-        familyTreeID: "",
+        patientID: "",
         id: ""
     })
     //callback is an array
@@ -109,7 +109,7 @@ exports.register = function(req, res, err) {
                     return console.error(err);
                 }
                 else  {
-                    res.json({ details , success: true, message:"Registered" })
+                    res.json({ details: details , success: true, message:"Registered" })
                 }
             })
         } else {
@@ -136,16 +136,18 @@ exports.login = function(req, res, err) {
                 return console.error(err);
             } else {
                 var userID = callback.id
+                var patientID = callback.patientID
+
                 console.log("callback.id "+ callback.id)
                 //callback is an array
-                console.log("callback.familyTreeID"+ callback.familyTreeID)
+                console.log("callback.patientID"+ callback.patientID)
 
-                FamilySchema.find({patientID: callback.familyTreeID}, function (err, callback) {
+                FamilySchema.find({patientID: callback.patientID}, function (err, callback) {
                     if (err) {
                         res.json({success: false, message:"Username used: "+err})
                         return console.error(err);
                     } else {
-                        res.json({userID: userID, familyTree: callback, success: true, message:"Logging in"})
+                        res.json({userID: userID, patientID: patientID, familyTree: callback, success: true, message:"Logging in"})
                     }
                 })
         }
